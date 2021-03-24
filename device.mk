26e7d727153884bd6b4454eca55e78ad3586b258
Albert Wang <albertccwang@google.com>
2021-03-24 22:56:18 +0800

    pdx206: Update USB HAL to V1.3 implementation
    Supports to enable/disable USB data signaling

--- device.mk
+++ device.mk
@@ -474,7 +474,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service.sonykona
+    android.hardware.usb@1.3-service.sonykona
 
 # Sensors
 PRODUCT_PACKAGES += \
