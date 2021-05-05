f6f1111a4c00351ed4160b3085283d187bd62d68
Georg Veichtlbauer <georg@vware.at>
2021-05-05 11:40:29 +0200

    sm8250-common: Remove errorneous Treble configuration
    * Packages are useless
    
    Partially reverts commit 38457a3dbcfec8c29f412a092e1bddd5463a08f8.
    
    Change-Id: I7d9b2d15b44ac01e3f690db69b137080faafd3bb

--- edo.mk
+++ edo.mk
@@ -251,7 +251,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libqdMetaData \
-    libvulkan \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
@@ -494,10 +493,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
-
-# Vendor libstdc++
-PRODUCT_PACKAGES += \
-    libstdc++.vendor
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
