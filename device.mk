ec2a903acffffb7d1b6abb8854fb829f4773d6a7
Akash Srivastava <akashniki@gmail.com>
2022-10-29 04:58:18 +0700

    Build some missing LDAC libs
    * Bluetooth libs look for them

--- device.mk
+++ device.mk
@@ -130,6 +130,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    libldacBT_abr \
+    libldacBT_bco \
+    libldacBT_enc \
     audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
