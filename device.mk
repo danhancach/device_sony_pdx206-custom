4035ffe190e49289f1ea4c0c85a8a560cfc184ad
johnmart19 <johnivan19999@gmail.com>
2022-10-28 03:03:15 +0300

    Do not balance msm_drm and kgsl-3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    [johnmart19]: Re-check that values are correct for sm8250:
    
    adb shell cat /proc/interrupts > interrupts.txt
    
    GICv3  27 Level     arch_timer
    GICv3  23 Level     arm-pmu
    GICv3  38 Level     arch_mem_timer
    GICv3 115 Level     msm_drm
    GICv3 332 Level     kgsl_3d0_irq
    
    Move msmirqbalance config to DT
    
    Co-Authored-By: johnmart19 <johnivan19999@gmail.com>

--- device.mk
+++ device.mk
@@ -358,6 +358,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
