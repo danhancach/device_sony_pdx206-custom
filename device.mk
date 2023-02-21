46f3b5bce3956e4ae79a513cec63806b876e06f2
tuan <tuan94_bg@outlook.com>
2023-02-22 08:22:15 +0900

    Update perf hal to 2.3

--- device.mk
+++ device.mk
@@ -474,7 +474,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power@1.3.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.3.vendor
 
 # QMI
 PRODUCT_PACKAGES += \
