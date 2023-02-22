0ce12f8388e4e8a9eb5b417f04ba048003621a46
tuan <tuan94_bg@outlook.com>
2023-02-22 11:40:46 +0900

    Reorganize bluetooth

--- device.mk
+++ device.mk
@@ -112,14 +112,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
     tinyplay \
     tinymix \
     audio_amplifier.kona
 
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
     audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
@@ -172,6 +170,13 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
+    libldacBT_abr \
+    libldacBT_bco \
+    libldacBT_enc
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
