e83adfd235c7276dc404f9fb02889e073b5d4a34
tuan <tuan94_bg@outlook.com>
2023-02-24 03:57:42 +0900

    nfc: Add config file for FeliCa (Type-F)

--- device.mk
+++ device.mk
@@ -465,6 +465,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp-typef.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-typef.conf \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 # OMX
