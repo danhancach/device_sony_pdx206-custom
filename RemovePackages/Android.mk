22ead86f8d703fd9497a7e79c48f5830d401f5b3
tuan <tuan94_bg@outlook.com>
2023-03-09 04:18:27 +0900

    Remove some gapps packages at build time

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -3,6 +3,27 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    ANGLE \
+    AmbientSensePrebuilt \
+    AppDirectedSMSService \
+    AndroidAutoStubPrebuilt \
+    BetterBugStub \
+    CalendarGooglePrebuilt \
+    CarrierLocation \
+    Chrome \
+    Chrome-Stub \
+    DeviceIntelligenceNetworkPrebuilt \
+    FilesPrebuilt \
+    GCS \
+    GoogleCamera \
+    GoogleTTS \
+    PlayAutoInstallConfig \
+    Velvet \
+    SCONE \
+    talkback \
+    TrichromeLibrary \
+    WebViewGoogle
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
