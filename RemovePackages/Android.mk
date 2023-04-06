8cf2d594ad8408561e2774919c38c296c805a320
danhancach <111163507+danhancach@users.noreply.github.com>
2023-04-07 00:01:01 +0900

    Remove some packages at build time

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -9,6 +9,21 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Aperture \
+    Chrome \
+    Chrome.apk.gz \
+    Chrome-Stub \
+    GoogleCamera \
+	Music \
+    MusicFX \
+    AudioFX \
+    Browser2 \
+    Calendar \
+    Contacts \
+    DeskClock \
+    Gallery2
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
