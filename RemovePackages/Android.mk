1a99f7c4db761231c6a713ee9f1b66145c8736b4
danhancach <111163507+danhancach@users.noreply.github.com>
2023-04-07 00:01:01 +0900

    Remove some packages at build time

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -9,6 +9,32 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    ANGLE \
+    AmbientSensePrebuilt \
+    AppDirectedSMSService \
+    Aperture \
+    BetterBugStub \
+    CalendarGooglePrebuilt \
+    CarrierLocation \
+    Chrome \
+    Chrome.apk.gz \
+    Chrome-Stub \
+    DeviceIntelligenceNetworkPrebuilt \
+    GCS \
+    GoogleCamera \
+    PlayAutoInstallConfig \
+    SCONE \
+    TrichromeLibrary \
+    WebViewGoogle \
+	Music MusicFX \
+    AudioFX \
+    Browser2 \
+    Calendar \
+    Contacts \
+    DeskClock \
+    Gallery2
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
