f2ba23110636f8e7411c72f62b56477ff1298480
danhancach <111163507+danhancach@users.noreply.github.com>
2023-04-08 11:00:24 +0900

    Switch to AIDL health HAL

--- device.mk
+++ device.mk
@@ -317,8 +317,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.example
 
 # HDR
 PRODUCT_COPY_FILES += \
