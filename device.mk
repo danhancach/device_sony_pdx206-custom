cfe328e8d3aaf4d38c31628d129eafafd1f1efe9
danhancach <111163507+danhancach@users.noreply.github.com>
2023-04-08 10:03:38 +0900

    Switch to AIDL memtrack HAL

--- device.mk
+++ device.mk
@@ -245,8 +245,7 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack-service.example \
     gralloc.kona \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
