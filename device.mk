3bd97ad93f84b38f37ada9c02f1e87a967e325a6
danhancach <tuan94_bg@outlook.com>
2023-05-12 13:54:28 +0900

    overlay: Reorganize

--- device.mk
+++ device.mk
@@ -44,6 +44,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
