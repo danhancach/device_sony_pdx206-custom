6c0049b5effb005aaf685b0846057cdb0f9ef67e
tuan <tuan94_bg@outlook.com>
2023-07-19 05:05:34 +0900

    sepolicy

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,12 @@
+# Sony Open Devices
+# SELinux policy variable definitions
+LOCAL_SEPOLICY := device/sony/pdx206/sepolicy
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(LOCAL_SEPOLICY)/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    $(LOCAL_SEPOLICY)/system_ext/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    $(LOCAL_SEPOLICY)/system_ext/public
