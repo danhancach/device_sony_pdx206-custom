1a6a707c7eb64bbce0539a5ecf98960022e753c4
Astridxx <muratkozan350@gmail.com>
2023-10-16 05:53:59 -0400

    Switch to NFC service AIDL

--- device.mk
+++ device.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2018 The LineageOS Project
 #
@@ -407,7 +408,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
+    android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
     android.hardware.secure_element@1.1.vendor \
     android.hardware.secure_element@1.2.vendor \
