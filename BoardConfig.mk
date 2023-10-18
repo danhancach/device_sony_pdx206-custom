ec74e8b7772395ec86adc782fe1b0d10c33c383d
tuan <tuan94_bg@outlook.com>
2023-10-19 08:48:57 +0900

    bring up to evox

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,7 +179,7 @@ TARGET_FS_CONFIG_GEN := $(CONFIGS_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(CONFIGS_PATH)/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/evolution/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(CONFIGS_PATH)/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(CONFIGS_PATH)/vintf/manifest.xml
 
